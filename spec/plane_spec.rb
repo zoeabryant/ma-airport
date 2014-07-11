934c458a585cec201a481faf05724c6992d18b47
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-07-11 11:50:54 +0100

    Initial set up, outline of basic plane functions

---
+++ spec/plane_spec.rb
@@ -0,0 +1,34 @@
+require 'plane'
+
+describe Plane do
+	it 'can take off from airport' do
+	end
+
+	it 'stays grounded if weather is bad' do
+	end
+
+	it 'knows that it is flying after it takes off' do
+	end
+
+	it 'can fly' do
+	end
+
+	it 'can land in airport' do
+	end
+
+	it 'stays flying if the weather is bad' do
+	end
+
+	it 'knows that it is not flying after it lands' do
+	end
+end
+
+
+
+ #              -=\`\
+ #          |\ ____\_\__
+ #        -=\c`""""""" "`)
+ #           `~~~~~/ /~~`
+ #             -==/ /
+ #               '-'
+ # © http://www.asciiworld.com/-Planes-.html :)
